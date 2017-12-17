166f3c57852ba564e70e74659365986846aebc50
Oreoluwa Banwo <oreoluwa.banwo@andela.com>
2017-12-17 17:59:41 +0100

    add support for specific episode download

--- o2tvseries/scripts/scraper.py
+++ o2tvseries/scripts/scraper.py
@@ -11,6 +11,7 @@ os.chdir(proj_path)
 # This is so models get loaded.
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+from clint.textui import progress
 from bs4 import BeautifulSoup
 import requests
 import re
@@ -20,7 +21,8 @@ from o2tvseries.models import Show, Season, Episode
 headers = {'user-agent': settings.USER_AGENT}
 
 
-prompts = {1: "Please enter show name: "}
+prompts = {1: "Please enter show name: ",
+           3: "Please enter the show_name, season and episode number as guided by the prompts"}
 
 def input():
     options = dict([(1, "New Show"),
@@ -98,11 +100,9 @@ def get_show_url(show_name):
         page_url = "{}/page{}.html".format(show_group_url, page_number)
         page_tags = get_a_tags(page_url)
         for tag in page_tags:
-            if tag.string:
-                print(tag.string.lower(), show_name_lower[0])
             checker = tag.string and show_name_lower and show_name_lower == tag.string.lower()
             if checker:
-                return tag.get('href')
+                return (tag.get('href'), tag.string)
             else:
                 checker = tag.string and show_name_list[0] and show_name_list[0] in tag.string.lower()
                 if len(show_name_list) > 1:
@@ -110,7 +110,7 @@ def get_show_url(show_name):
                 if len(show_name_list) > 2:
                     checker = checker and show_name_list[2] and show_name_list[2] in tag.string.lower()
                 if checker:
-                    return tag.get('href')
+                    return (tag.get('href'), tag.string)
 
     raise ValueError("Show: {} cannot be found at {}".format(show_name, settings.SOURCE_URL))
 
@@ -123,7 +123,7 @@ def find_last_page(index_page_a_tags):
 
 
 def get_show_seasons(show_name):
-    show_url = get_show_url(show_name)
+    show_url, _ = get_show_url(show_name)
     tags = get_a_tags(show_url)
     seasons = []
     for tag in tags:
@@ -134,6 +134,12 @@ def get_show_seasons(show_name):
 
 
 def get_episodes(show_name, season):
+    """
+    Gets the list of episode urls for the supplied show and season
+    :param show_name: a string of the show to be downloaded
+    :param season: an int of the season to download
+    :return: a sorted list of show episode urls
+    """
     seasons = get_show_seasons(show_name)
     try:
         season_number = int(season)
@@ -153,7 +159,6 @@ def get_episodes(show_name, season):
 
     for page_number in range(1, int(last_page_no) + 1):
         page_url = "{}/page{}.html".format(season_home_url[:-11], page_number)
-        print page_url
         page_tags = get_a_tags(page_url)
         for tag in page_tags:
             checker = tag.string and re.search('episode\s\d+', tag.string.lower())
@@ -162,6 +167,12 @@ def get_episodes(show_name, season):
     return sorted(episodes, key=lambda x: int(re.search("episode\s(\d+)", x[0].lower()).group(1)))
 
 def get_referrer_link(episode_url, format='mp4'):
+    """
+    Method that obtains referrer link of supplied episode
+    :param episode_url: url for episode to be downloaded
+    :param format: format of episode to be downloaded 3gp/mp4
+    :return: a 2-Tuple containing referrer link and filename
+    """
     tags = get_a_tags(episode_url)
     result = {}
 
@@ -169,19 +180,65 @@ def get_referrer_link(episode_url, format='mp4'):
         if tag.string:
             if tag.string.lower().endswith(format):
                 if format == 'mp4':
+                    hd = lq = ""
                     if 'HD' in tag.string:
-                        result[format] = tag.get('href')
-                    elif not result.get(format):
-                        result[format] = tag.get('href')
+                        hd = (tag.get('href'), tag.string)
+                    else:
+                        lq = (tag.get('href'), tag.string)
+                    result[format] = hd if hd else lq
                 else:
-                    result[format] = tag.get('href')
+                    result[format] = (tag.get('href'), tag.string)
     return result[format]
 
+def download_file(referrer_link, download_path, file_name, show_name, season):
+    print referrer_link, download_path, file_name, show_name, season
+    folder_path = "{}/{}/{}".format(download_path, show_name, season)
+    full_file_save_path = "{}/{}".format(folder_path, file_name)
+
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        os.chmod(folder_path, 0o777)
+
+    result = requests.get(str(referrer_link), stream=True, headers=headers)
+
+    with open(full_file_save_path, 'wb') as f:
+        file_size = int(result.headers.get('content-length'))
+        for chunk in progress.bar(result.iter_content(chunk_size=1024), expected_size=(file_size / 1024) + 1):
+            if chunk:
+                f.write(chunk)
+                f.flush()
+
+def download_specific_episode(action_item):
+    print prompts[action_item]
+    show_name = raw_input("Enter the Show Name eg. 'Arrow': ")
+    _, proper_show_name = get_show_url(show_name)
+    seasons = get_show_seasons(show_name)
+    season_number = raw_input(
+        "This show has {no_of_seasons} seasons, Please enter a number from 1 - {no_of_seasons}: ".format(
+            no_of_seasons=len(seasons)))
+    try:
+        season = seasons[int(season_number) - 1][0]
+
+        episodes = get_episodes(show_name, season_number)
+
+        episode_no = raw_input("Please enter the episode No: ")
+
+        episode_no = int(episode_no)
+
+        episode_no = episode_no if len(str(episode_no)) > 1 else '0{}'.format(episode_no)
+        for url in episodes:
+            if ('Episode' in url[0] and str(episode_no) in url[0]):
+                print url, episode_no
+        episode_url = [url[1] for url in episodes if ('Episode' in url[0] and str(episode_no) in url[0])][0]
+    except:
+        print "Invalid Entry, please try again"
+        raise
+    referrer_link, file_name = get_referrer_link(episode_url)
+    download_file(referrer_link, settings.DOWNLOAD_PATH, file_name, proper_show_name, season)
 
 
 def new_show(input):
-    url = get_show_url(input)
-
+    url, _ = get_show_url(input)
 
 
 if __name__ == "__main__":
@@ -189,4 +246,6 @@ if __name__ == "__main__":
     if action_item == 1:
         input_value = raw_input(prompts[action_item])
         for episode, url in get_episodes(input_value, 1):
-            print get_referrer_link(url, '3gp')
+            print get_referrer_link(url, 'mp4')
+    elif action_item == 3:
+        download_specific_episode(action_item)
--- tvseriesdownloaddjango/settings.py
+++ tvseriesdownloaddjango/settings.py
@@ -122,6 +122,6 @@ STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
 
-DOWNLOAD_PATH = "/Users/"
+DOWNLOAD_PATH = "/Users/oreoluwa/Downloads/TVshows"
 SOURCE_URL = "http://o2tvseries.com"
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Ubuntu/11.10 Chromium/27.0.1453.93 Chrome/27.0.1453.93 Safari/537.36"
