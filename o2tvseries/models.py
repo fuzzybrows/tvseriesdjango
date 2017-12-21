03a64dc2a9a4192692d448cf3be6fa585d552865
Oreoluwa Banwo <oreoluwa.banwo@andela.com>
2017-12-21 19:21:52 +0100

    update models

--- o2tvseries/models.py
+++ o2tvseries/models.py
@@ -7,20 +7,25 @@ from django.db import models
 
 class Show(models.Model):
     title = models.CharField(max_length=255)
-    base_url = models.CharField(max_length=255)
+    show_url = models.CharField(max_length=255)
     active = models.BooleanField(default=False)
 
 class Season(models.Model):
     title = models.CharField(max_length=255)
+    season_no = models.IntegerField()
     show = models.ForeignKey(Show)
+    season_url = models.CharField(max_length=100)
 
 class Episode(models.Model):
     show = models.ForeignKey(Show)
     season = models.ForeignKey(Season)
-    episode_no = models.CharField(max_length=50)
-    episode_url = models.CharField(max_length=50)
-    referrer_url = models.CharField(max_length=50)
-    filename = models.CharField(max_length=50)
+    episode_title  = models.CharField(max_length=100)
+    episode_url = models.CharField(max_length=100)
+    referrer_url = models.CharField(max_length=100)
+    filename = models.CharField(max_length=100)
+    file_format = models.CharField(max_length=10)
+    save_location = models.CharField(max_length=100)
     download_size = models.IntegerField()
     download_timestamp = models.DateTimeField(auto_now_add=True)
+    downloaded = models.BooleanField(default=False)
 
